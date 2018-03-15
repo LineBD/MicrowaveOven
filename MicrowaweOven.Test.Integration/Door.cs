17a4b8a86de1b8142be17167ecf95fa085f9e145
bodil <bodil@crone.dk>
2018-03-15 11:53:33 +0100

    door integration start

---
+++ MicrowaweOven.Test.Integration/Door.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace MicrowaweOven.Test.Integration
+{
+    [TestFixture]
+    public class Door
+    {
+        private UserInterface _userinterface;
+
+        public void Setup ()
+        {
+            _userinterface = new UserInterface(new IButton(),new IButton(),  );
+        }
+
+    }
+}
