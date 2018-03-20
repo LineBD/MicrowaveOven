059083527c1c33bf2180ea2a5e49d22e98e2768a
bodil <bodil@crone.dk>
2018-03-20 08:46:35 +0100

    button færdig

--- MicrowaweOven.Test.Integration/ButtonIntegration.cs
+++ MicrowaweOven.Test.Integration/ButtonIntegration.cs
@@ -45,13 +45,70 @@ namespace MicrowaweOven.Test.Integration
         }
 
         [Test]
-        public void TimerOn_TimerPressed_ShowTime()
+        [TestCase(01,00)]
+        public void TimerOn_TimerPressed_ShowTime(int min, int sec)
         {
+            _powerButton.Press();
             _timerButton.Press();
 
             _display.Received(1).ShowTime(01,00);
 
-            //Hvorfor fungerer den ikke, når det kører over?
         }
+
+        [Test]
+        public void TurnOnMicrowave_StartCancelPressed_LightTurnedOn()
+        {
+            _powerButton.Press();
+            _timerButton.Press();
+            _startcancelButton.Press();
+
+            _light.Received(1).TurnOn();
+        }
+
+    
+        [Test]
+        public void TurnOnMicrowave_StartCancelPressed_StartCooking()
+        {
+            _powerButton.Press();
+            _timerButton.Press();
+            _startcancelButton.Press();
+
+            _controller.Received(1).StartCooking(50,60);
+        }
+
+        [Test]
+        public void Cancel_StartCancelPressedWhileCooking_ClearDisplay()
+        {
+            _powerButton.Press();
+            _timerButton.Press();
+            _startcancelButton.Press();
+            _startcancelButton.Press();
+
+            _display.Received(2).Clear();
+        }
+
+        [Test]
+        public void Cancel_StartCancelPressedWhileCooking_StopCooking()
+        {
+            _powerButton.Press();
+            _timerButton.Press();
+            _startcancelButton.Press();
+            _startcancelButton.Press();
+
+            _controller.Received(1).Stop();
+        }
+
+        [Test]
+        public void Cancel_StartCancelPressedWhileCooking_TurnOffLight()
+        {
+            _powerButton.Press();
+            _timerButton.Press();
+            _startcancelButton.Press();
+            _startcancelButton.Press();
+
+            _light.Received(1).TurnOff();
+        }
+
+
     }
 }
