94f055c26c03d941fc05a7478f2ac670d068b14c
Rikke <201607161@post.au.dk>
2018-03-22 11:48:04 +0100

    sdfg

--- MicrowaweOven.Test.Integration/IT2_LightIntegration.cs
+++ MicrowaweOven.Test.Integration/IT2_LightIntegration.cs
@@ -11,48 +11,48 @@ using NUnit.Framework;
 
 namespace MicrowaweOven.Test.Integration
 {
-    [TestFixture]
-    class IT2_LightIntegration
-    {
-        private IUserInterface _userinterface;
-        private IButton _startcancelButton;
-        private IButton _powerButton;
-        private IButton _timerButton;
-        private IDoor _door;
-        private ILight _light;
-        private IDisplay _display;
-        private ICookController _controller;
-        private IOutput _output;
+    //[TestFixture]
+    //class IT2_LightIntegration
+    //{
+    //    private IUserInterface _userinterface;
+    //    private IButton _startcancelButton;
+    //    private IButton _powerButton;
+    //    private IButton _timerButton;
+    //    private IDoor _door;
+    //    private ILight _light;
+    //    private IDisplay _display;
+    //    private ICookController _controller;
+    //    private IOutput _output;
 
-        [SetUp]
-        public void SetUp()
-        {
-            _door = new Door();
-            _output = Substitute.For<IOutput>();
-            _light = new Light(_output);
-            _display = Substitute.For<IDisplay>();
-            _controller = Substitute.For<ICookController>();
-            _startcancelButton = new Button();
-            _powerButton = new Button();
-            _timerButton = new Button();
-            _userinterface = new UserInterface(_powerButton, _timerButton, _startcancelButton, _door, _display, _light,
-                _controller);
-        }
+    //    [SetUp]
+    //    public void SetUp()
+    //    {
+    //        _door = new Door();
+    //        _output = Substitute.For<IOutput>();
+    //        _light = new Light(_output);
+    //        _display = Substitute.For<IDisplay>();
+    //        _controller = Substitute.For<ICookController>();
+    //        _startcancelButton = new Button();
+    //        _powerButton = new Button();
+    //        _timerButton = new Button();
+    //        _userinterface = new UserInterface(_powerButton, _timerButton, _startcancelButton, _door, _display, _light,
+    //            _controller);
+    //    }
 
-        [Test]
-        public void TurnOn_LightIsOn_OutputIsCorrect()
-        {
-            _light.TurnOn();
-            _output.Received(1).OutputLine("Light is turned on");
-            //Dette er unittest, vi intergrer ikke med systemet. Vi vælger ikke at lave en integretionstest af Light, da vi tester på outputtet i UIIntegration. 
-        }
+    //    [Test]
+    //    public void TurnOn_LightIsOn_OutputIsCorrect()
+    //    {
+    //        _light.TurnOn();
+    //        _output.Received(1).OutputLine("Light is turned on");
+    //        //Dette er unittest, vi intergrer ikke med systemet. Vi vælger ikke at lave en integretionstest af Light, da vi tester på outputtet i UIIntegration. 
+    //    }
 
-        [Test]
-        public void TurnOff_LightIsOff_OutputIsCorrect()
-        {
-            _light.TurnOn();
-            _light.TurnOff();
-            _output.Received(1).OutputLine("Light is turned off");
-        }
-    }
+    //    [Test]
+    //    public void TurnOff_LightIsOff_OutputIsCorrect()
+    //    {
+    //        _light.TurnOn();
+    //        _light.TurnOff();
+    //        _output.Received(1).OutputLine("Light is turned off");
+    //    }
+    //}
 }
